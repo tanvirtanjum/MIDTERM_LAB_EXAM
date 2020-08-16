47b693f95f94fcecea140b8cfbd79cc161a69405
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-08-16 13:55:09 +0600

    admin update delete

--- controller/admin.js
+++ controller/admin.js
@@ -26,14 +26,161 @@ router.post('/', function(req, res)
 {
 	if(req.body.hasOwnProperty("add"))
 	{
-		//res.redirect('admin/addemployee');
+		res.redirect('admin/addemployee');
 	}
 
 	if (req.body.hasOwnProperty("all"))
 	{
-		//res.redirect('admin/allemployeelist');
+		res.redirect('admin/allemployeelist');
+	}
+});
+
+router.get('/addemployee', function(req, res)
+{
+	if(req.session.type == 1)
+	{
+		res.render('admin/addemployee/index');
+	}
+	else
+	{
+		res.redirect("/login");
+	}
+});
+
+router.post('/addemployee', function(req, res)
+{
+	var user=
+	{
+		username: req.body.uname,
+		fname: req.body.fname,
+		phone: req.body.phone,
+		status: req.body.des,
+		pass: req.body.pass
 	}
 
+	log_in.addLogin(user, function(resp)
+	{
+		if(resp)
+		{
+			res.redirect('/admin/allemployeelist');
+		}
+
+		else {
+			res.send("False");
+		}
+	})
+});
+router.get('/allemployeelist', function(req, res)
+{
+	if(req.session.type == 1)
+	{
+		log_in.getALL(function(result)
+		{
+			res.render('admin/allemployeelist/index', {list: result});
+		});
+	}
+	else
+	{
+		res.redirect("/login");
+	}
+});
+router.get('/update/:id', function(req, res)
+{
+	user=
+	{
+		username: req.params.id
+	}
+	log_in.getINFO(user,function(result)
+	{
+		res.render('admin/allemployeelist/update/index', {list: result, err: err});
+	});
+});
+
+router.post('/update/:id', function(req, res)
+{
+	var user=
+	{
+		fname:  req.body.p,
+		pass:  req.body.b,
+		con:  req.body.c,
+		username: req.body.n
+	}
+
+	var e = false;
+	if(user.fname.length < 1)
+	{
+		//console.log("null");
+		err.a="*";
+		e = true;
+	}
+	else
+	{
+		err.a="";
+	}
+	if(user.pass.length < 1)
+	{
+		err.b="*";
+		e = true;
+	}
+	else
+	{
+		err.b="";
+	}
+	if(user.con.length < 11 || user.con.length > 11)
+	{
+		err.c="*";
+		e = true;
+	}
+	else
+	{
+		err.c="";
+	}
+
+	if(!e)
+	{
+		log_in.up(user, function(resp)
+		{
+			res.redirect('/admin/allemployeelist');
+		});
+	}
+	else
+	{
+		res.redirect('/admin/update/'+req.params.id);
+	}
+});
+
+router.get('/delete/:id', function(req, res)
+{
+	user=
+	{
+		username: req.params.id
+	}
+	log_in.getINFO(user,function(result)
+	{
+		res.render('admin/allemployeelist/delete/index', {list: result});
+	});
+});
+
+router.post('/delete/:id', function(req, res)
+{
+	if(req.body.hasOwnProperty("y"))
+	{
+		var user=
+		{
+			fname:  req.body.p,
+			pass:  req.body.b,
+			username: req.body.n
+		}
+		log_in.del(req.params.id, function(resp)
+		{
+			res.redirect('/admin/allemployeelist');
+		});
+	}
+
+	else if (req.body.hasOwnProperty("n"))
+	{
+		res.redirect('/admin/allemployeelist');
+	}
 });
 
 
