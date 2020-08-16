6724da4e942747772bb89777208ddf6b1ced9233
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-08-16 21:16:40 +0600

    Registration Page

---
+++ controller/reg.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+var log_in 	= require.main.require('./models/log_in');
+
+router.get('/', function(req, res)
+{
+	res.render('reg/index');
+});
+
+router.post('/', function(req, res)
+{
+	var user=
+	{
+		username: req.body.uname,
+		fname: req.body.fname,
+		phone: req.body.phone,
+		status: req.body.des,
+		pass: req.body.pass
+	}
+
+	log_in.addLogin(user, function(resp)
+	{
+		if(resp)
+		{
+			res.redirect('/login');
+		}
+
+		else
+    {
+			res.send("False");
+		}
+	})
+});
+
+module.exports = router;
